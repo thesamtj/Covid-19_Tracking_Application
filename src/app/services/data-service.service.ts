0d6cbfbbbb42d3b0af585dd123ea1e47c7f712bc
Samuel Tijani <omalitijani@gmail.com>
2020-06-22 15:44:41 +0100

    Add response type

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataServiceService {
+  private globalDataUrl =
+    'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/06-21-2020.csv';
 
-  private globalDataUrl = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/06-21-2020.csv";
-
-  constructor(private http: HttpClient ) { }
+  constructor(private http: HttpClient) {}
 
   getGlobalData() {
-    return this.http.get(this.globalDataUrl).pipe(
-      map(result=>{
-      })
-    )
+    return this.http
+      .get(this.globalDataUrl, { responseType : 'text' })
+      .pipe(map((result) => {}));
   }
 }
