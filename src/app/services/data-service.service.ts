48da3a3d0f089104a13ce20b877ef1b9767fc4f7
Samuel Tijani <omalitijani@gmail.com>
2020-06-22 15:31:40 +0100

    Add url

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataServiceService {
 
+  private globalDataUrl = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/06-21-2020.csv";
+
   constructor(private http: HttpClient ) { }
 
   getGlobalData() {
-    
+
   }
 }
