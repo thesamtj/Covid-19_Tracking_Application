bf118ee3c6895915ec1eee8ee9642740370fb318
Samuel Tijani <omalitijani@gmail.com>
2020-06-23 22:48:07 +0100

    Aggregate the data by countries

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -16,20 +16,35 @@ export class DataServiceService {
     return this.http.get(this.globalDataUrl, { responseType: 'text' }).pipe(
       map((result) => {
         let data: GlobalDataSummary[] = [];
+        let raw = {};
         let rows = result.split('\n');
         rows.splice(0, 1);
+
         rows.forEach((row) => {
           let cols = row.split(/,(?=\S)/);
-          data.push({
+
+          let cs = {
             country: cols[3],
             confirmed: +cols[7],
             deaths: +cols[8],
             recovered: +cols[9],
             active: +cols[10],
-          });
+          };
+          let temp: GlobalDataSummary = raw[cs.country];
+          if (temp) {
+            temp.active = cs.active + temp.active;
+            temp.confirmed = cs.confirmed + temp.confirmed;
+            temp.deaths = cs.deaths + temp.deaths;
+            temp.recovered = cs.recovered + temp.recovered;
+
+            raw[cs.country] = temp;
+          } else {
+            raw[cs.country] = cs;
+          }
+         
         });
 
-        console.log(data);
+        console.log(raw);
         return [];
       })
     );
