a8da47d482c5cabe52ae43d0e4a6ff216740d6eb
Samuel Tijani <omalitijani@gmail.com>
2020-06-24 10:24:19 +0100

    Extract countries

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from 'src/app/services/data-service.service';
+import { GlobalDataSummary } from 'src/app/models/global-data';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.scss']
+  styleUrls: ['./countries.component.scss'],
 })
 export class CountriesComponent implements OnInit {
+  data: GlobalDataSummary[];
+  countries: string[] = [];
 
-  constructor() { }
+  constructor(private dataService: DataServiceService) {}
 
   ngOnInit(): void {
+    this.dataService.getGlobalData().subscribe((result) => {
+      this.data = result;
+      this.data.forEach((cs) => {
+        this.countries.push(cs.country);
+      });
+    });
   }
-
 }
