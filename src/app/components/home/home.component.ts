97961229410d3d38922736da8dcb7d8b32ae5c96
Samuel Tijani <omalitijani@gmail.com>
2020-06-24 09:17:44 +0100

    Check for NaN

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,31 +4,30 @@ import { DataServiceService } from 'src/app/services/data-service.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   totalConfirmed = 0;
   totalActive = 0;
   totalDeaths = 0;
-  totalRecovered= 0;
+  totalRecovered = 0;
 
-  constructor(private dataService: DataServiceService) { }
+  constructor(private dataService: DataServiceService) {}
 
   ngOnInit(): void {
-    this.dataService.getGlobalData().subscribe(
-      {
-        next: (result) => {
-          console.log(result);
+    this.dataService.getGlobalData().subscribe({
+      next: (result) => {
+        console.log(result);
 
-          result.forEach((cs) =>{
+        result.forEach((cs) => {
+          if (!Number.isNaN(cs.confirmed)) {
             this.totalConfirmed += cs.active;
             this.totalConfirmed += cs.confirmed;
             this.totalDeaths += cs.deaths;
             this.totalRecovered += cs.recovered;
-          })
-        }
-      }
-    )
+          }
+        });
+      },
+    });
   }
-
 }
