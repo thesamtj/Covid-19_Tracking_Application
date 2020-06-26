27ec5ab92721b616cec9c7384a88272b94b2f8a5
Samuel Tijani <omalitijani@gmail.com>
2020-06-26 21:05:08 +0100

    Change to Google Charts Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Ng2GoogleChartsModule, GoogleChartsSettings } from 'ng2-google-charts';
+import { GoogleChartsModule } from 'angular-google-charts';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CountriesComponent } from './components/countries/countries.component';
 import { DashboardCardComponent } from './components/dashboard-card/dashboard-card.component';
-
-// const MyGoogleChartsSettings: GoogleChartsSettings = {
-//   mapsApiKey: 'YOUR_API_KEY',
-//   googleChartsVersion: '46',
-//   language: 'it',
-// };
 
 @NgModule({
   declarations: [
@@ -22,20 +16,15 @@ import { DashboardCardComponent } from './components/dashboard-card/dashboard-ca
     HomeComponent,
     NavbarComponent,
     CountriesComponent,
-    DashboardCardComponent
+    DashboardCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    Ng2GoogleChartsModule
+    GoogleChartsModule,
   ],
-  providers: [
-    // {
-    //   provide: 'googleChartsSettings',
-    //   useValue: MyGoogleChartsSettings,
-    // },
-  ],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
