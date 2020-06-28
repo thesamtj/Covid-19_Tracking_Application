b2a466ab9810fd0ef2c4f812e6f952d36a092e88
Samuel Tijani <omalitijani@gmail.com>
2020-06-28 11:47:11 +0100

    Change output path

--- server/server.js
+++ server/server.js
@@ -1,16 +1,29 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('../dist/corona-tracker'));
+// get dist folder
+const distDir = path.join(__dirname, "../dist");
 
-app.get('/*', (req, res)=> {
-    
-res.sendFile(path.join(__dirname,'../dist/corona-tracker/index.html'));
-});
+// use dist folder as hosting folder by express
+app.use(express.static(distDir));
+
+// // Serve only the static files form the dist directory
+// app.use(express.static('../dist/corona-tracker'));
+
+// serve the index.html
+app.get("*", (req, res) => res.sendFile(path.join(distDir, "index.html")));
+
+// app.get('/*', (req, res)=> {
+
+// res.sendFile(path.join(__dirname,'../dist/corona-tracker/index.html'));
+// });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`server is running from port ${port}`);
+});
